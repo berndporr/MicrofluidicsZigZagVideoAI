11bbfb0ad5c925fbdab66de3237675eb04d50627
Bernd Porr <bernd.porr@glasgow.ac.uk>
2022-11-26 14:55:20 +0000

    FrameGenerator coding for the ZigZag completed but still buggy.
    Followed the guidelines at https://www.tensorflow.org/tutorials/load_data/video
    to ceate a Frame Generator which spits out pairs of videos and labels.
    Runs without crash but most of the returned videos are empty. Needs
    more debugging.
    
    Also: Given up on loading the entire 1G clip into memory as TF
    seems to have a memory limit of about 1.5G. Tried loading it as
    a np.array but then the 1G clips baloons up about 16G in memory.
    Now opening both files in openCV as two classes and then
    loading them from disk during training. TF seems to be doing its
    own caching anyway.

--- loadvideo.py
+++ loadvideo.py
@@ -20,95 +20,94 @@ def format_frames(frame, output_size):
     Return:
       Formatted frame with padding of specified output size.
   """
-  print("output size",output_size)
-  print(f"Frame type {type(frame)}")
   frame = tf.image.convert_image_dtype(frame, tf.float32)
   #frame = tf.image.resize_with_pad(frame, *output_size)
   return frame
 
 
-def all_frames_from_video_file(video_path, output_scaling = False, frame_step = 1):
-  """
-    Creates frames from each video file present for each category.
+class AVIfile:
 
-    Args:
-      video_path: File path to the video.
-      n_frames: Number of frames to be created per video file.
-      output_size: Pixel size of the output frame image.
+  def __init__(self, video_path, label_name, clip_length = 30, scaling = False):
+    self.clip_length = clip_length
+    self.scaling = False;
+    self.label_name = label_name
+    self.src = cv2.VideoCapture(str(video_path))  
+    self.video_length = int(self.src.get(cv2.CAP_PROP_FRAME_COUNT))
+    self.width  = int(self.src.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
+    self.height = int(self.src.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
+    print("{} opened: {} frames, {} clips at {}x{}".format(video_path,self.video_length,self.get_number_of_clips(),
+    self.width,self.height))
 
-    Return:
-      An NumPy array of frames in the shape of (n_frames, height, width, channels).
-  """
-  # Read each video frame by frame
-  result = []
-  print("Opening:"+video_path)
-  src = cv2.VideoCapture(str(video_path))  
+  def get_number_of_clips(self):
+    return self.video_length // self.clip_length
 
-  video_length = int(src.get(cv2.CAP_PROP_FRAME_COUNT))
-  width  = src.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-  height = src.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+  def get_frames_of_clip(self, clip_index, frame_step = 1):
+    if (self.scaling):
+      output_size = (int(self.width * self.output_scaling), int(self.height * self.output_scaling))
+    else:
+      output_size = (self.width, self.height)
 
-  print("Capture of length={} {}x{}".format(video_length,width,height))
+    self.src.set(cv2.CAP_PROP_POS_FRAMES, clip_index * self.clip_length)
 
-  if (output_scaling):
-    output_size = (int(width * output_scaling), int(height * output_scaling))
-  else:
-    output_size = (width, height)
+    result = []
 
-  src.set(cv2.CAP_PROP_POS_FRAMES, 0)
-  # ret is a boolean indicating whether read was successful, frame is the image itself
-  ret, frame = src.read()
-  print("Result:",ret,frame)
-  result.append(format_frames(frame, output_size))
+    ret, frame = self.src.read()
+    result.append(frame)
 
-  for i in range(video_length - 1):
-    print(i)
-    ret, frame = src.read()
-    if ((i+1) % frame_step) == 0:
-      if ret:
-        frame = format_frames(frame, output_size)
-        result.append(frame)
-      else:
-        result.append(np.zeros_like(result[0]))
-  src.release()
-  result = np.array(result)[..., [2, 1, 0]]
+    for i in range(1,self.clip_length):
+      ret, frame = self.src.read()
+      if (i % frame_step) == 0:
+        if ret:
+          result.append(frame)
+        else:
+          result.append(np.zeros_like(result[0]))
+    self.src.release()
+    ## returning the array but fixing openCV's odd BGR format to RGB
+    result = np.array(result)[...,[2,1,0]]
 
-  return result
+    return result
 
 
 class FrameGenerator:
-  def __init__(self, path, n_frames, training = False):
-    """ Returns a set of frames with their associated label. 
-
-      Args:
-        path: Video file paths.
-        n_frames: Number of frames. 
-        training: Boolean to determine if training dataset is being created.
-    """
-    self.path = path
-    self.n_frames = n_frames
+  def __init__(self, avifiles, clips_list, training = False):
+    self.avifiles = avifiles
     self.training = training
-    self.class_names = sorted(set(p.name for p in self.path.iterdir() if p.is_dir()))
-    self.class_ids_for_name = dict((name, idx) for idx, name in enumerate(self.class_names))
-
-  def get_files_and_class_names(self):
-    video_paths = list(self.path.glob('*/*.avi'))
-    classes = [p.parent.name for p in video_paths] 
-    return video_paths, classes
+    self.clips_list = clips_list
 
   def __call__(self):
-    video_paths, classes = self.get_files_and_class_names()
-
-    pairs = list(zip(video_paths, classes))
+    pairs = []
+    for clip_index in self.clips_list:
+      for label_index in range(len(self.avifiles)):
+        pair = (label_index,clip_index)
+        pairs.append(pair)
 
     if self.training:
       random.shuffle(pairs)
 
-    for path, name in pairs:
-      video_frames = all_frames_from_video_file(path) 
-      label = self.class_ids_for_name[name] # Encode labels
+    for label_index,clip_index in pairs:
+      video_frames = self.avifiles[label_index].get_frames_of_clip(clip_index)
+      label = label_index
       yield video_frames, label
 
+path_to_healthy = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0005.avi"
+path_to_ill = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0004.avi"
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-all_frames_from_video_file("/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0004.avi")
+avi_healthy = AVIfile(path_to_healthy,"Healthy")
+avi_ill  = AVIfile(path_to_ill,"Ill")
+
+avi_files = [avi_healthy,avi_ill]
+
+healthy_first_clip = avi_healthy.get_frames_of_clip(0)
+#print(healthy_first_clip)
+#print(healthy_first_clip.shape)
+
+train_clips_list = range(0,300)
+val_clips_list = range(300,450)
+test_clips_list = range(450,600)
+
+
+fg = FrameGenerator(avi_files, train_clips_list, training=True)
+
+for i in range(3):
+  frames, label = next(fg())
+  print(f"i:{i}, Shape: {frames.shape}, Label: {label}")
