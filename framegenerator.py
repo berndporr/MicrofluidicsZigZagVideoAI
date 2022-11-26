3ef85518be85983ea9ffe8d9fb2fe6a91ac175f8
Bernd Porr <bernd.porr@glasgow.ac.uk>
2022-11-26 17:02:50 +0000

    Split framegenerator and test
    Image import seems to be working now and random seek.
    Shapes of 3 matrices looks exactly as they should
    and imshow generates the correct image so import looks fine.
    
    Todo: playing back of a full clip, cropping and 1st/last frame
    in a clip.

--- framegenerator.py
+++ framegenerator.py
@@ -31,12 +31,17 @@ class AVIfile:
     self.clip_length = clip_length
     self.scaling = False;
     self.label_name = label_name
-    self.src = cv2.VideoCapture(str(video_path))  
+    self.src = cv2.VideoCapture(str(video_path))
+    if not self.src:
+      print("Could not open:",video_path)
     self.video_length = int(self.src.get(cv2.CAP_PROP_FRAME_COUNT))
     self.width  = int(self.src.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
     self.height = int(self.src.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
     print("{} opened: {} frames, {} clips at {}x{}".format(video_path,self.video_length,self.get_number_of_clips(),
     self.width,self.height))
+
+  def __del__(self):
+    self.src.release()
 
   def get_number_of_clips(self):
     return self.video_length // self.clip_length
@@ -47,11 +52,17 @@ class AVIfile:
     else:
       output_size = (self.width, self.height)
 
-    self.src.set(cv2.CAP_PROP_POS_FRAMES, clip_index * self.clip_length)
+    framepos =  clip_index * self.clip_length
+    # print("Going to frame pos:",framepos)
+    self.src.set(cv2.CAP_PROP_POS_FRAMES,framepos)
 
     result = []
 
     ret, frame = self.src.read()
+    if not ret:
+      print("Frame read error")
+      quit()
+
     result.append(frame)
 
     for i in range(1,self.clip_length):
@@ -61,7 +72,6 @@ class AVIfile:
           result.append(frame)
         else:
           result.append(np.zeros_like(result[0]))
-    self.src.release()
     ## returning the array but fixing openCV's odd BGR format to RGB
     result = np.array(result)[...,[2,1,0]]
 
@@ -88,26 +98,3 @@ class FrameGenerator:
       video_frames = self.avifiles[label_index].get_frames_of_clip(clip_index)
       label = label_index
       yield video_frames, label
-
-path_to_healthy = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0005.avi"
-path_to_ill = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0004.avi"
-
-avi_healthy = AVIfile(path_to_healthy,"Healthy")
-avi_ill  = AVIfile(path_to_ill,"Ill")
-
-avi_files = [avi_healthy,avi_ill]
-
-healthy_first_clip = avi_healthy.get_frames_of_clip(0)
-#print(healthy_first_clip)
-#print(healthy_first_clip.shape)
-
-train_clips_list = range(0,300)
-val_clips_list = range(300,450)
-test_clips_list = range(450,600)
-
-
-fg = FrameGenerator(avi_files, train_clips_list, training=True)
-
-for i in range(3):
-  frames, label = next(fg())
-  print(f"i:{i}, Shape: {frames.shape}, Label: {label}")
---
+++ test_framegenerator.py
@@ -0,0 +1,32 @@
+import framegenerator
+
+import matplotlib.pyplot as plt
+
+path_to_healthy = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0005.avi"
+path_to_ill = "/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0004.avi"
+
+avi_healthy = framegenerator.AVIfile(path_to_healthy,"Healthy")
+avi_ill  = framegenerator.AVIfile(path_to_ill,"Ill")
+
+avi_files = [avi_healthy,avi_ill]
+
+healthy_first_clip = avi_healthy.get_frames_of_clip(0)
+#print(healthy_first_clip)
+#print(healthy_first_clip.shape)
+
+train_clips_list = range(0,300)
+val_clips_list = range(300,450)
+test_clips_list = range(450,600)
+
+
+fg = framegenerator.FrameGenerator(avi_files, train_clips_list, training=True)
+
+for i in range(3):
+  frames, label = next(fg())
+  print(f"i:{i}, Shape: {frames.shape}, Label: {label}")
+  img = frames[0]
+  print(img.shape)
+
+frames, label = next(fg())
+plt.imshow(frames[0])
+plt.show()
