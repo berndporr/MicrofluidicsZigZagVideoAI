8a96104ff83970006a6650f69a79365727e1a65c
Bernd Porr <bernd.porr@glasgow.ac.uk>
2022-12-08 19:21:10 +0000

    Testopencv now also plays a couple of video frames.

--- testopencv.py
+++ testopencv.py
@@ -9,6 +9,10 @@ import collections
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
 
 src = cv2.VideoCapture("/data/ZigZag-Channel-beads4_5um/60xPhotron_C001H001S0005.avi")
 video_length = int(src.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -16,9 +20,18 @@ width  = int(src.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
 height = int(src.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
 print(width,"x",height)
 
-for i in range(10):
+fig, ax = plt.subplots()
+ims = []
+
+for i in range(100):
       ret, frame = src.read()
-      print(frame)
-      print(ret)
+      im = ax.imshow(frame, animated=True)
+      ims.append([im])
 
 src.release()
+
+ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                repeat_delay=0)
+
+
+plt.show()
