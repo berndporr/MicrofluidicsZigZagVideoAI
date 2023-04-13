a0f1632c063b95241b30750f1bb488f1ea75306c
Raj <r.b.rajkumar.007@gmail.com>
2023-04-13 13:26:49 +0100

    Centered the bg_sub RBC. Chose 100 frames for training instead of 250. Testing the model by saving weights after epoch.

--- classifier_working.py
+++ classifier_working.py
@@ -1,9 +1,6 @@
 import os
 import time
 import random
-import os
-import time
-import random
 import logging
 from typing import Tuple
 
@@ -13,9 +10,10 @@ import tensorflow as tf
 from tqdm import tqdm
 from keras.applications import EfficientNetB0
 from keras.models import Model
-from keras.layers import LSTM, TimeDistributed, Conv2D, MaxPooling2D, Flatten, Dense, Input, GlobalAveragePooling2D
+from keras.layers import LSTM, TimeDistributed, Conv2D, \
+    MaxPooling2D, Flatten, Dense, Input, GlobalAveragePooling2D, Dropout, Bidirectional
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from keras import mixed_precision
 from sklearn.utils import shuffle
 from tqdm.keras import TqdmCallback
@@ -34,56 +32,20 @@ def setup_gpu_memory_growth() -> None:
 
 def main():
     setup_gpu_memory_growth()
-
+    if tf.test.is_gpu_available():
+        print("TensorFlow is running on GPU")
+    else:
+        print("TensorFlow is running on CPU")
 
 if __name__ == '__main__':
     main()
-
-
-import cv2
-import numpy as np
-
-
-class RBCExtractor:
-    def __init__(self, target_shape=(224, 224, 3), min_blob_size=50, max_blob_size=500, min_circularity=0.5):
-        self.target_shape = target_shape
-        self.min_blob_size = min_blob_size
-        self.max_blob_size = max_blob_size
-        self.min_circularity = min_circularity
-        self.bg_subtractor = BackgroundSubtractor()
-
-    def extract(self, frame):
-        # Apply background subtraction
-        fg_mask = self.bg_subtractor.apply(frame)
-
-        # Find blobs in the foreground mask
-        detector = cv2.SimpleBlobDetector_create()
-        keypoints = detector.detect(fg_mask)
-        rbc_keypoints = []
-        for kp in keypoints:
-            # Filter blobs based on size and circularity
-            if self.min_blob_size < kp.size < self.max_blob_size and kp.size / kp.angle > self.min_circularity:
-                rbc_keypoints.append(kp)
-
-        # Create square regions around the detected RBCs
-        regions = []
-        for kp in rbc_keypoints:
-            x, y = int(kp.pt[0]), int(kp.pt[1])
-            size = int(kp.size * 0.8)  # 80% of the original size
-            x1, y1 = max(0, x - size // 2), max(0, y - size // 2)
-            x2, y2 = min(frame.shape[1] - 1, x + size // 2), min(frame.shape[0] - 1, y + size // 2)
-            region = frame[y1:y2, x1:x2, :]
-            region = cv2.resize(region, self.target_shape[:2], interpolation=cv2.INTER_AREA)
-            regions.append(region)
-
-        # Return the resized regions
-        return np.array(regions)
 
 
 class BackgroundSubtractor:
     def __init__(self):
         self.bg_model = None
         self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=30, varThreshold=10)
+        self.bbox = None
 
     def apply(self, frame: np.ndarray) -> np.ndarray:
         # Preprocess the frame
@@ -113,20 +75,19 @@ class BackgroundSubtractor:
         combined_mask = cv2.bitwise_and(fg_mask, bg_mask)
 
         # Dilate the combined mask
-        kernel_dilation = np.ones((8, 8), np.uint8)
-        combined_mask = cv2.dilate(combined_mask, kernel_dilation, iterations=3)
+        kernel_dilation = np.ones((7, 7), np.uint8)
+        combined_mask = cv2.dilate(combined_mask, kernel_dilation, iterations=2)
         combined_mask = cv2.dilate(combined_mask, kernel, iterations=1)
         combined_mask = cv2.erode(combined_mask, kernel, iterations=1)
 
-        # Extract the bounding box of the largest connected component
         contours, hierarchy = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) > 0:
             areas = [cv2.contourArea(c) for c in contours]
             max_index = np.argmax(areas)
             x, y, w, h = cv2.boundingRect(contours[max_index])
-            bbox = (x, y, w, h)
+            self.bbox = (x, y, w, h)
         else:
-            bbox = (0, 0, frame.shape[1], frame.shape[0])
+            self.bbox = (0, 0, frame.shape[1], frame.shape[0])
 
         # Extract the foreground
         fg = cv2.bitwise_and(frame, frame, mask=combined_mask)
@@ -138,7 +99,7 @@ bg_subtractor = BackgroundSubtractor()
 
 
 class FrameGenerator:
-    def __init__(self, batch_size: int = 16, shuffle: bool = True, target_shape: Tuple[int, int, int] = (224, 224, 3)):
+    def __init__(self, batch_size: int = 16, shuffle: bool = True, target_shape: Tuple[int, int, int] = (112, 112, 3)):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.target_shape = target_shape
@@ -171,27 +132,46 @@ class FrameGenerator:
             yield batch
 
     @staticmethod
-    def resize_frame(frame, target_shape):
-        # Crop or pad the frame to make it a square
+    def resize_and_center_frame(frame, target_shape, bbox):
         h, w = frame.shape[:2]
+        max_dim = max(h, w)
+        pad_top, pad_bottom, pad_left, pad_right = 0, 0, 0, 0
+        x, y, w, h = bbox
+
+        # Crop the region of interest
+        roi = frame[y:y + h, x:x + w]
+
+        # Calculate padding for the cropped frame to make it square
         if h > w:
-            pad_top = 0
-            pad_bottom = 0
             pad_left = (h - w) // 2
             pad_right = h - w - pad_left
         else:
-            pad_left = 0
-            pad_right = 0
             pad_top = (w - h) // 2
             pad_bottom = w - h - pad_top
-        preprocessed_frame = cv2.copyMakeBorder(frame, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=0)
 
-        # Resize the framel
-        resized_frame = cv2.resize(preprocessed_frame, (target_shape[1], target_shape[0]), interpolation=cv2.INTER_AREA)
+        # Pad the cropped frame
+        padded_roi = cv2.copyMakeBorder(roi, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=0)
 
-        return resized_frame
+        # Resize the padded_roi to fill 80% of the target_shape
+        scale = min(target_shape[0] * 0.8 / h, target_shape[1] * 0.8 / w)
+        new_h, new_w = int(h * scale), int(w * scale)
+        resized_roi = cv2.resize(padded_roi, (new_w, new_h), interpolation=cv2.INTER_AREA)
 
-frame_gen = FrameGenerator()
+        # Pad the resized_roi to the final target_shape
+        pad_top = (target_shape[0] - new_h) // 2
+        pad_bottom = target_shape[0] - new_h - pad_top
+        pad_left = (target_shape[1] - new_w) // 2
+        pad_right = target_shape[1] - new_w - pad_left
+        final_frame = cv2.copyMakeBorder(resized_roi, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=0)
+
+        return final_frame
+
+    @staticmethod
+    def resize_frame(frame, target_shape, bbox=None):
+        if bbox is not None:
+            # Apply the original resizing
+            preprocessed_frame = FrameGenerator.resize_and_center_frame(frame, target_shape, bbox)
+        return preprocessed_frame
 
 
 frame_gen = FrameGenerator()
@@ -214,22 +194,30 @@ def process_videos(video_paths, bg_subtractor, target_shape, max_frames=None):
             logging.warning(f"Could not open video file: {video_path}")
             continue
         video_frames = []
+        frame_count = 0
         while True:
             ret, frame = cap.read()
             if ret and frame.size > 0:  # check that frame is not empty:
                 preprocessed_frame = bg_subtractor.apply(frame)
-                resized_frame = FrameGenerator.resize_frame(preprocessed_frame, target_shape)
-                video_frames.append(resized_frame)
-                # if not os.path.exists('/home/raj/PycharmProjects/frames'):
-                #     os.makedirs('/home/raj/PycharmProjects/frames')
-                # cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_orig.jpg', frame)
-                # cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_bgsub.jpg', preprocessed_frame)
-                # cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_resized.jpg', resized_frame)
+                bbox = bg_subtractor.bbox
+                # Remove the first and last 25 frames
+                if 10 <= frame_count < 210:
+                    # Skip one frame after each frame
+                    if (frame_count - 25) % 2 == 0:
+                        resized_frame = FrameGenerator.resize_frame(preprocessed_frame, target_shape, bbox)
+                        video_frames.append(resized_frame)
+                        if not os.path.exists('/home/raj/PycharmProjects/frames'):
+                            os.makedirs('/home/raj/PycharmProjects/frames')
+                        # cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_orig.jpg', frame)
+                        # #cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_bgsub.jpg', preprocessed_frame)
+                        # cv2.imwrite('/home/raj/PycharmProjects/frames/' + str(label) + str(len(video_frames)).zfill(3) + '_resized.jpg', resized_frame)
+                frame_count += 1
             else:
                 break
         cap.release()
 
-        if not video_frames:
+        # Skip videos that do not have RBC flowing through the channel
+        if len(video_frames) == 0:
             continue
 
         if max_frames is not None:
@@ -264,6 +252,7 @@ def process_videos(video_paths, bg_subtractor, target_shape, max_frames=None):
     print(f"Random video label: {video_label}, file path: {video_path}")
 
     return X, y, num_frames, np.array(labels_list)
+
 
 def count_videos(path_list):
     num_videos = 0
@@ -305,16 +294,21 @@ def split_videos(path_list, train_split):
         num_train = int(train_split * len(videos))
         train_paths.extend([os.path.join(path, v) for v in videos[:num_train]])
         val_paths.extend([os.path.join(path, v) for v in videos[num_train:]])
+    random.shuffle(train_paths)
+    random.shuffle(val_paths)
     return train_paths, val_paths
 
 
 # Split healthy and ill videos into train and validation sets
 healthy_train, healthy_val = split_videos(healthy_paths, train_split)
+print("Shuffled: training videos")
 ill_train, ill_val = split_videos(ill_paths, train_split)
+print("Shuffled: validation videos")
+time.sleep(1)
 
 # Limit the dataset to 1500 healthy/ill train and 250 healthy/ill val
-healthy_train = random.sample(healthy_train, 100)
-ill_train = random.sample(ill_train, 100)
+healthy_train = random.sample(healthy_train, 45)
+ill_train = random.sample(ill_train, 45)
 
 healthy_val = random.sample(healthy_val, 5)
 ill_val = random.sample(ill_val, 5)
@@ -327,7 +321,7 @@ print(f"{len(ill_val)} ill validation videos sent to the model")
 time.sleep(1)
 
 # Define the target shape of the resized video frames
-target_shape = (224, 224, 3)
+target_shape = (112, 112, 3)
 
 
 def preprocess_data(healthy_data, ill_data, target_shape, num_samples):
@@ -346,6 +340,7 @@ def preprocess_data(healthy_data, ill_data, target_shape, num_samples):
 
     return data_resized, labels
 
+
 # Preprocess the training data
 train_data_resized, train_labels = preprocess_data(healthy_train, ill_train, target_shape=(224, 224, 3), num_samples=4)
 
@@ -357,12 +352,11 @@ print("train_labels shape:", train_labels.shape, "dtype:", train_labels.dtype)
 print("val_data_resized shape:", val_data_resized.shape, "dtype:", val_data_resized.dtype)
 print("val_labels shape:", val_labels.shape, "dtype:", val_labels.dtype)
 
-
-
+# ----------------------------------- #
 
 
 # Load pre-trained EfficientNetB0 model
-base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(112, 112, 3))
 
 # Add a GlobalAveragePooling2D layer to convert 4D tensor to 3D tensor
 global_avg_pool = GlobalAveragePooling2D()(base_model.output)
@@ -371,41 +365,71 @@ global_avg_pool = GlobalAveragePooling2D()(base_model.output)
 for layer in base_model.layers:
     layer.trainable = False
 
-inputs = Input(shape=(250, 224, 224, 3))
+inputs = Input(shape=(100, 224, 224, 3))
 time_distributed_base_model = TimeDistributed(base_model)(inputs)
 
-x = TimeDistributed(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))(time_distributed_base_model)
+x = TimeDistributed(Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))(time_distributed_base_model)
 x = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(x)
+x = TimeDistributed(Dropout(0.5))(x)
 x = TimeDistributed(Flatten())(x)
 
 # Pass the 3D tensor to the LSTM layer
-x = LSTM(units=64, activation='relu')(x)
+x = Bidirectional(LSTM(units=32, activation='tanh', recurrent_activation='sigmoid', return_sequences=True))(x)
+x = Dropout(0.5)(x)
+x = Bidirectional(LSTM(units=32, activation='tanh', recurrent_activation='sigmoid'))(x)
 predictions = Dense(units=1, activation='sigmoid')(x)
 
 # Create the model
 model = Model(inputs=inputs, outputs=predictions)
 
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
-
 # Define batch size and number of epochs
 epochs = 5
-batch_size = 32
+batch_size = 8
 
-# Train the model with tqdm progress bars
-history = model.fit(
-    train_data_resized,
-    train_labels,
-    batch_size=batch_size,
-    epochs=epochs,
-    validation_data=(val_data_resized, val_labels),
-    callbacks=[
-        EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True),
-        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, min_lr=0.0001),
-        TqdmCallback(verbose=1)
-    ],
-    verbose=0
+# Define the filepath to save the model weights
+checkpoint_filepath = '/home/raj/PycharmProjects/weights/model_weights.h5'
+print("Saved: model weights")
+
+# Define the callback to save the model weights after each epoch
+checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    save_best_only=False,
+    mode='auto',
+    save_freq='epoch'
 )
+
+# Load saved weights
+model.load_weights(checkpoint_filepath)
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.002), loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+chunk_size = 10  # Adjust the chunk size as needed
+num_chunks = train_data_resized.shape[0] // chunk_size
+
+for chunk_index in range(num_chunks):
+    start_idx = chunk_index * chunk_size
+    end_idx = (chunk_index + 1) * chunk_size
+
+    train_data_chunk = train_data_resized[start_idx:end_idx]
+    train_labels_chunk = train_labels[start_idx:end_idx]
+
+    history = model.fit(
+        train_data_chunk,
+        train_labels_chunk,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(val_data_resized, val_labels),
+        callbacks=[
+            EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True),
+            ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, min_lr=0.0001),
+            checkpoint_callback,
+            TqdmCallback(verbose=1, position=0, leave=True),
+        ],
+        verbose=0
+    )
 
 # Evaluate the model on the validation set
 val_loss, val_acc = model.evaluate(val_data_resized, val_labels, verbose=0)
@@ -413,5 +437,5 @@ print('Validation loss:', val_loss)
 print('Validation accuracy:', val_acc)
 
 # Print model summary and trainable parameters
-print(model.summary(include_all=False))
+print(model.summary())
 print("Trainable parameters:", model.count_params())
