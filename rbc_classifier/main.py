4dec5d5e20b83ddb74878fea63a894378cf0cf2c
Raj <r.b.rajkumar.007@gmail.com>
2023-05-04 14:49:54 +0100

    Updated np to tensor conversion, added function comments, and addressing model input shape incompatibility.

--- rbc_classifier/main.py
+++ rbc_classifier/main.py
@@ -2,20 +2,24 @@ import getopt
 import sys
 import logging
 import time
-from video_processor import get_videos, preprocess_data
+from video_processor import get_videos, process_dataset
 import tensorflow as tf
-import numpy as np
-from keras.utils import to_categorical
 
+# Set default values for parameters
+frame_height = 132
+frame_width = 800
+num_channels = 3
+NUM_FRAMES = 100
 TARGET_SHAPE = (132, 800, 3)
+
 
 def main(argv):
     # Set default values for parameters
-    train_videos = 10
-    val_videos = 2
-    epochs = 5
+    train_videos = 18
+    val_videos = 4
+    epochs = 10
     batch_size = 1
-
+    # Get parameters from command line
     try:
         opts, args = getopt.getopt(argv, "t:v:e:b:", ["train_videos=", "val_videos=", "epochs=", "batch_size="])
     except getopt.GetoptError:
@@ -41,7 +45,7 @@ def main(argv):
         if physical_devices:
             for device in physical_devices:
                 tf.config.experimental.set_memory_growth(device, True)
-        print(f"{len(physical_devices)} GPU(s) available")
+        print(f"{len(physical_devices)} GPU available")
 
     def main():
         setup_gpu_memory_growth()
@@ -73,110 +77,59 @@ def main(argv):
                  "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Overfocused",
                  "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Underfocused"]
 
-    # Select videos for healthy and ill training and validation sets
+    # healthy_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/healthy"]
+    # ill_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/mod2"]
+
+    # Select videos for training and validation sets
     healthy_train_paths, healthy_train_labels = get_videos(healthy_paths, label=1, num_videos=train_videos // 2)
     ill_train_paths, ill_train_labels = get_videos(ill_paths, label=0, num_videos=train_videos // 2)
 
     healthy_val_paths, healthy_val_labels = get_videos(healthy_paths, label=1, num_videos=val_videos // 2)
     ill_val_paths, ill_val_labels = get_videos(ill_paths, label=0, num_videos=val_videos // 2)
 
-    print(f"{len(healthy_train_paths)} healthy training videos sent to the model")
-    print(f"{len(ill_train_paths)} ill training videos sent to the model")
-    # time.sleep(1)
-    print("")
+    # Process the data
+    t_videos, t_labels = process_dataset(healthy_train_paths, ill_train_paths,
+                                            healthy_train_labels, ill_train_labels)
 
-    print(f"{len(healthy_val_paths)} healthy validation videos sent to the model")
-    print(f"{len(ill_val_paths)} ill validation videos sent to the model")
-    # time.sleep(1)
-    print("")
+    v_videos, v_labels = process_dataset(healthy_val_paths, ill_val_paths,
+                                        healthy_val_labels, ill_val_labels)
 
-    # Preprocess the training data
-    train_data_resized, train_labels = preprocess_data(healthy_train_paths, ill_train_paths,
-                                                       healthy_train_labels, ill_train_labels,
-                                                       target_shape=TARGET_SHAPE, num_samples=train_videos)
+    # Convert to tensors
+    train_ds = tf.data.Dataset.zip((t_videos, t_labels))
+    train_ds = train_ds.shuffle(buffer_size=1000)
+    train_ds = train_ds.batch(batch_size)
+    train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
 
-    # Preprocess the validation data
-    val_data_resized, val_labels = preprocess_data(healthy_val_paths, ill_val_paths,
-                                                   healthy_val_labels, ill_val_labels,
-                                                   target_shape=TARGET_SHAPE, num_samples=val_videos)
-
-    train_paths = healthy_train_paths + ill_train_paths
-    val_paths = healthy_val_paths + ill_val_paths
-
-    print("")
-    time.sleep(1)
-    print("train_data_resized shape:", train_data_resized.shape, "dtype:", train_data_resized.dtype)
-    print("train_labels shape:", train_labels.shape, "dtype:", train_labels.dtype)
-    print("")
-    # time.sleep(1)
-
-    # Print 5 random training videos and labels
-    print("Random training videos and labels:")
-    for _ in range(5):
-        idx = np.random.choice(train_videos)
-        label = train_labels[idx]
-        video_path = train_paths[idx]
-        print("Label:", label, "Video path:", video_path)
-    print("")
-    time.sleep(1)
-
-    print("val_data_resized shape:", val_data_resized.shape, "dtype:", val_data_resized.dtype)
-    print("val_labels shape:", val_labels.shape, "dtype:", val_labels.dtype)
-    print("")
-    # time.sleep(1)
-
-    # Print 5 random validation videos and labels
-    print("Random validation videos and labels:")
-    for _ in range(5):
-        idx = np.random.choice(val_videos)
-        label = val_labels[idx]
-        video_path = val_paths[idx]
-        print("Label:", label, "Video path:", video_path)
-    print("")
-    time.sleep(1)
+    val_ds = tf.data.Dataset.zip((v_videos, v_labels))
+    val_ds = val_ds.shuffle(buffer_size=1000)
+    val_ds = val_ds.batch(batch_size)
+    val_ds = val_ds.prefetch(tf.data.experimental.AUTOTUNE)
 
     # ----------------------------------- #
 
-    # Define the input shape of the video frames
-    num_frames = 100
-    frame_height = 132
-    frame_width = 800
-    num_channels = 3
-
-    inputs = tf.keras.layers.Input(shape=(num_frames, frame_height, frame_width, num_channels))
-
-    # Load the EfficientNetB0 model without the top layer
-    net = tf.keras.applications.EfficientNetB0(include_top=False)
-    net.trainable = False
-
-    # Add custom layers for classification
-    x = tf.keras.layers.TimeDistributed(net)(inputs)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    x = tf.keras.layers.Dense(10, activation='relu')(x)
-    x = tf.keras.layers.GlobalAveragePooling3D()(x)
-    predictions = tf.keras.layers.Dense(2, activation='softmax')(x)
-
-    # Finalize the model
-    model = tf.keras.models.Model(inputs=inputs, outputs=predictions)
-
-    # Set the optimizer with a smaller learning rate
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    # optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
+    # Build the model
+    model = tf.keras.Sequential([
+        tf.keras.applications.EfficientNetB0(include_top=False),
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1, activation='sigmoid'),
+    ])
 
     # Compile the model
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    train_labels = to_categorical(train_labels, num_classes=2)
-    val_labels = to_categorical(val_labels, num_classes=2)
-
-    train_ds = tf.data.Dataset.from_tensor_slices((train_data_resized, train_labels)).batch(batch_size)
-    val_ds = tf.data.Dataset.from_tensor_slices((val_data_resized, val_labels)).batch(batch_size)
+    # Print the model summary
+    model.summary()
+    time.sleep(5)
 
     # Train the model
     history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, verbose=1, validation_data=val_ds)
+
     print("")
-    accuracy = history.history['accuracy'][0] * 100
+    accuracy = history.history['accuracy'][-1] * 100
     print('Accuracy: {:.2f}%'.format(accuracy))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- rbc_classifier/video_processor.py
+++ rbc_classifier/video_processor.py
@@ -5,102 +5,107 @@ import random
 from background_subtractor import BackgroundSubtractor
 import numpy as np
 import os
+import tensorflow as tf
+import time
 
-NUM_FRAMES = 100
-bg_subtractor = BackgroundSubtractor()
 
-def process_videos(video_paths_labels, bg_subtractor, target_shape, max_frames=None):
+def get_videos(path_list, label, num_videos):
+    # Initialize variables.
+    video_paths = []
+    video_labels = []
+    all_videos = []
+
+    # Iterate over the paths.
+    for path in path_list:
+        # Get a list of video paths in the directory.
+        videos = [os.path.join(path, f)
+                  for f in os.listdir(path)
+                  if f.endswith('.avi') and os.path.isfile(os.path.join(path, f))]
+
+        # Add the video paths to the list of all videos.
+        all_videos.extend(videos)
+
+    # Shuffle the list of all videos.
+    random.shuffle(all_videos)
+
+    # Get the first `num_videos` videos from the list of all videos.
+    selected_videos = all_videos[:num_videos]
+
+    # Iterate over the selected videos.
+    for video in selected_videos:
+        # Add the video path and label to the lists of video paths and labels.
+        video_paths.append(video)
+        video_labels.append(label)
+
+    # Return the lists of video paths and labels.
+    return video_paths, video_labels
+
+
+def process_dataset (healthy_data, ill_data, healthy_labels, ill_labels):
+    # Process the videos.
+    healthy_data_processed = process_videos(healthy_data)
+    ill_data_processed = process_videos(ill_data)
+
+    # Concatenate the healthy and ill data.
+    processed_videos = np.concatenate([healthy_data_processed, ill_data_processed], axis=0)
+    processed_videos = processed_videos.astype(np.float32) / 255.0
+    processed_videos = tf.data.Dataset.from_tensor_slices(processed_videos)
+
+    # Use the provided labels.
+    labels = np.concatenate([healthy_labels, ill_labels], axis=0)
+    labels = labels.astype(np.int16)
+    labels = tf.data.Dataset.from_tensor_slices(labels)
+
+    # Return the processed data and labels.
+    return processed_videos, labels
+
+
+def process_videos(video_paths_labels):
+    # Initialize variables.
     processed_videos = []
-    labels_list = []
+    bg_subtractor = BackgroundSubtractor()
 
-    for video_path, label in tqdm(video_paths_labels, desc='Processing videos', position=0, leave=True):
+    # Iterate over the video paths and labels.
+    for video_path in tqdm(video_paths_labels, desc='Processing videos', position=0, leave=True):
+        # Open the video file.
         cap = cv2.VideoCapture(video_path)
         if not cap.isOpened():
             logging.warning(f"Could not open video file: {video_path}")
             continue
 
+        # Get the number of frames in the video.
+        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # Initialize the video frames list.
         video_frames = []
 
-        for frame_count in range(160):
+        # Iterate over the frames in the video.
+        for frame_count in range(num_frames):
             ret, frame = cap.read()
             if not ret or frame.size == 0:
                 break
 
-            if 39 < frame_count < 160:
+            # Apply background subtraction to the frame.
+            if 40 < frame_count <= 160:
                 preprocessed_frame = bg_subtractor.apply(frame)
 
-                if frame_count > 49 and frame_count < 150:  # Skip first and last 10 frames
+                # Skip the first and last 10 frames.
+                if 50 < frame_count <= 150:
                     video_frames.append(preprocessed_frame)
-                    # cv2.imwrite(f'/home/raj/PycharmProjects/frames/{label}_{frame_count:03}_preprocessed.jpg', preprocessed_frame)
+                    # cv2.imwrite(f'/home/raj/PycharmProjects/frames/
+                    # {label}_{frame_count:03}_preprocessed.jpg', preprocessed_frame)
 
+        # Close the video file.
         cap.release()
 
+        # If there are no frames in the video, skip it.
         if len(video_frames) == 0:
             continue
 
-        if max_frames is not None:
-            num_frames = len(video_frames)
-            if num_frames < max_frames:
-                padding = np.zeros((max_frames - num_frames, *target_shape[1:]), dtype=np.uint8)
-                video_frames.extend([padding] * (max_frames - num_frames))
-            elif num_frames > max_frames:
-                video_frames = video_frames[:max_frames]
+        # Stack the frames into a NumPy array.
+        processed_videos.append(np.stack(video_frames, axis=0))
 
-        video_data = np.stack(video_frames, axis=0)
-        # print("Video data shape:", video_data.shape)
-        processed_videos.append((video_data, label, video_data.shape[0]))
-        labels_list.append(label)
+    video_data = processed_videos
 
-    X = np.array([video[0] for video in processed_videos])
-    y = np.array([video[1] for video in processed_videos])
-    num_frames = np.array([video[2] for video in processed_videos])
-
-    X = X.astype(np.float32) / 255.0
-
-    idx = np.random.randint(len(processed_videos))
-    video_label = processed_videos[idx][1]
-    video_path = video_paths_labels[idx]
-    print(f"{video_path}")
-
-    return X, y, num_frames, np.array(labels_list)
-
-def get_videos(path_list, label, num_videos):
-    video_paths = []
-    video_labels = []
-    all_videos = []
-
-    for path in path_list:
-        videos = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.avi') and os.path.isfile(os.path.join(path, f))]
-        all_videos.extend(videos)
-
-    random.shuffle(all_videos)
-    selected_videos = all_videos[:num_videos]
-
-    for video in selected_videos:
-        video_paths.append(video)
-        video_labels.append(label)
-
-    return video_paths, video_labels
-
-def preprocess_data(healthy_data, ill_data, healthy_labels, ill_labels, target_shape, num_samples):
-    healthy_video_paths_labels = list(zip(healthy_data, healthy_labels))
-    ill_video_paths_labels = list(zip(ill_data, ill_labels))
-
-    healthy_data_resized, _, num_healthy_frames, _ = process_videos(healthy_video_paths_labels, bg_subtractor, target_shape)
-    ill_data_resized, _, num_ill_frames, _ = process_videos(ill_video_paths_labels, bg_subtractor, target_shape)
-
-    # Concatenate the healthy and ill data
-    data_resized = np.concatenate([healthy_data_resized, ill_data_resized], axis=0)
-
-    # Get the number of input videos
-    num_videos = data_resized.shape[0]
-
-    # Reshape the data to the format (num_videos, num_frames, frame_height, frame_width, num_channels)
-    data_resized = data_resized.reshape(num_videos, NUM_FRAMES, *target_shape, order='C')
-    data_resized = data_resized[:num_samples].astype(np.float32)
-
-    # Use the provided labels
-    labels = np.concatenate([healthy_labels, ill_labels])
-    labels = labels[:num_samples]
-
-    return data_resized, labels
+    # Return the processed videos, labels, number of frames, and labels.
+    return video_data
