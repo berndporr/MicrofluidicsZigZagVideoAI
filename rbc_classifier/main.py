bca2a949a3a71b4f9bb5b39246c952a7c4da71e8
raj <r.b.rajkumar.007@gmail.com>
2023-05-22 05:12:23 +0100

    Saving as separate files for overlays and bar charts, added video selection options: ALL, FD, DA.

--- rbc_classifier/main.py
+++ rbc_classifier/main.py
@@ -1,17 +1,12 @@
 import sys
-import random
 import getopt
 import logging
-import time
 from video_processor import get_videos, process_dataset
 import plots
-import cv2
 
 import tensorflow as tf
-import numpy as np
 from keras.models import Sequential
 from keras.applications import EfficientNetB0
-from keras.callbacks import EarlyStopping
 from keras.models import save_model, load_model
 
 from keras.losses import SparseCategoricalCrossentropy
@@ -20,28 +15,38 @@ import matplotlib.pyplot as plt
 
 
 def main(argv):
-    # Set default values for parameters
-    videos = 10
-    epochs = 10
-    batch_size = 1
+    videos = 0
+    epochs = 0
+    option = "TEST"  # Initialize option with the default value
 
     # Get parameters from command line
     try:
-        opts, args = getopt.getopt(argv, "v:e:", ["videos=", "epochs="])
+        opts, args = getopt.getopt(argv, "", ["option="])
     except getopt.GetoptError:
-        print("main.py -v <videos> -e <epochs>")
+        print("main.py --option=<option_name>")
         sys.exit(2)
 
     for opt, arg in opts:
-        if opt in ("-v", "--videos"):
-            videos = int(arg)
-        elif opt in ("-e", "--epochs"):
-            epochs = int(arg)
+        if opt == "--option":
+            option = arg
+
+    if option == "ALL":
+        videos = 300
+        epochs = 300
+    elif option == "FD":
+        videos = 100
+        epochs = 100
+    elif option == "DA":
+        videos = 100
+        epochs = 100
+    elif option == "TEST":
+        videos = 20
+        epochs = 10
 
     train_index = int(videos * 0.5)
     val_index = int(train_index + (videos * 0.1))
-    test_index = int(val_index + (videos * 0.3))
-    video_index = int ((train_index + val_index + test_index) // 2)
+    test_index = int(val_index + (videos * 0.2))
+    video_index = int((train_index + val_index + test_index) // 2)
 
     # Disable all logging messages
     logging.disable(logging.CRITICAL)
@@ -68,46 +73,51 @@ def main(argv):
     if __name__ == '__gpu__':
         gpu()
 
-    print(f"{int(videos)} videos chosen.")
-    print(f"{epochs} epochs chosen.")
     print("")
-    # time.sleep(1)
+    print(f"{int(videos)} videos and {epochs} epochs chosen")
+    print("")
 
-    # # Define the healthy and ill paths
+    # Define the healthy and ill paths
 
-    native_paths = ["/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_3_focused",
-                      "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_2_underfocused",
-                      "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_4_overfocused"]
+    native_paths = [
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/Native5_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_overfocused2ticks",
+         "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_underfocused2ticks",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_3_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_2_underfocused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_4_overfocused"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/Native5_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_overfocused2ticks",
+         "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_underfocused2ticks"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_3_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_2_underfocused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC_9March2023_Donor2_4_overfocused"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/Native5_focused"]
+    ]
 
-    # "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_focused",
-    #                       "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_overfocused2ticks",
-    #                       "/data/RBC_Phantom_60xOlympus/Donor_1/Native5_underfocused2ticks"
+    modified_paths = [
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/FA_0.37wtPercent",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Overfocused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Underfocused"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/FA_0.37wtPercent"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_focused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Overfocused",
+         "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Underfocused"],
+        ["/data/RBC_Phantom_60xOlympus/Donor_1/FA_0.37wtPercent"]
+    ]
 
-    modified_paths = ["/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_focused",
-                        "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Overfocused",
-                        "/data/RBC_Phantom_60xOlympus/Donor_2/RBC10March2023_Donor2_2ndDay_1mMDiamide_Split_Underfocused"]
+    # Validate the chosen option
+    if option not in ["ALL", "FD", "DA", "TEST"]:
+        print("Invalid option. Choosing the default option.")
+        option = "ALL"
 
-    native_videos, native_labels = get_videos(native_paths, label=1, num_videos=video_index)
-    modified_videos, modified_labels = get_videos(modified_paths, label=0, num_videos=video_index)
+    # Get the selected paths based on the chosen option
+    selected_native_paths = native_paths[["ALL", "FD", "DA", "TEST"].index(option)]
+    selected_modified_paths = modified_paths[["ALL", "FD", "DA", "TEST"].index(option)]
 
-    # "/data/RBC_Phantom_60xOlympus/Donor_1/FA_0.37wtPercent"
-    #
-
-    # v_native_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/native_mix"]
-    # v_modified_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/mod_mix"]
-    #
-    # test_native_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/nat"]
-    # test_modified_paths = ["/home/raj/PycharmProjects/droplets_video/rbc_classifier/mod"]
-
-    # # Get the videos and labels.
-    # train_native_videos, train_native_labels = get_videos(native_paths, label=1, num_videos=train_index)
-    # train_modified_videos, train_modified_labels = get_videos(modified_paths, label=0, num_videos=train_index)
-    #
-    # val_native_videos, val_native_labels = get_videos(native_paths, label=1, num_videos=val_index)
-    # val_modified_videos, val_modified_labels = get_videos(modified_paths, label=0, num_videos=val_index)
-    #
-    # test_native_videos, test_native_labels = get_videos(native_paths, label=1, num_videos=test_index)
-    # test_modified_videos, test_modified_labels = get_videos(modified_paths, label=0, num_videos=test_index)
+    native_videos, native_labels = get_videos(selected_native_paths, label=1, num_videos=video_index)
+    modified_videos, modified_labels = get_videos(selected_modified_paths, label=0, num_videos=video_index)
 
     # Split the videos and labels into train, validation, and test sets.
     train_native_videos, train_native_labels = native_videos[:train_index], native_labels[:train_index]
@@ -127,21 +137,20 @@ def main(argv):
     test_videos_tensor, test_labels_tensor = process_dataset(test_native_videos, test_modified_videos,
                                                              test_native_labels, test_modified_labels)
 
-
     # Process the dataset into a form that can be used by the model
     autotune = tf.data.experimental.AUTOTUNE
 
     train_dataset = tf.data.Dataset.zip((train_videos_tensor, train_labels_tensor))
     train_dataset.cache().shuffle(10).prefetch(buffer_size=autotune)
-    train_dataset = train_dataset.batch(batch_size)
+    train_dataset = train_dataset.batch(1)
 
     val_dataset = tf.data.Dataset.zip((val_videos_tensor, val_labels_tensor))
     val_dataset.cache().prefetch(buffer_size=autotune)
-    val_dataset = val_dataset.batch(batch_size)
+    val_dataset = val_dataset.batch(1)
 
     test_dataset = tf.data.Dataset.zip((test_videos_tensor, test_labels_tensor))
     test_dataset.cache().prefetch(buffer_size=autotune)
-    test_dataset = test_dataset.batch(batch_size)
+    test_dataset = test_dataset.batch(1)
 
     # ----------------------------------- #
 
@@ -151,37 +160,33 @@ def main(argv):
 
     # Create a sequential model.
     model = Sequential([
-        # Rescaling the input to the range [0, 1].
+        # Rescaling the input to the range [0, 1]
         Rescaling(scale=255),
-        # TimeDistributed wrapper to apply the same transformation to each sample in the batch.
+        # TimeDistributed wrapper to apply the same transformation to each sample in the batch
         TimeDistributed(base_model),
-        # Dropout layer to prevent overfitting.
+        # Dropout layer to prevent over-fitting
         Dropout(0.2),
-        # Dense layer to output the probability of each class.
+        # Dense layer to output the probability of each class
         Dense(10),
-        # Pooling layer to reduce the dimensionality of the input.
+        # Pooling layer to reduce the dimensionality of the input
         GlobalAveragePooling3D()
     ])
 
-    # Compile the model.
+    # Compile the model
     model.compile(optimizer='adam',
                   loss=SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
-    # Fit the model to the training dataset and validation data.
-    history = model.fit(train_dataset, epochs=epochs, batch_size=batch_size, validation_data=val_dataset)
+    # Fit the model to the training dataset and validation data
+    history = model.fit(train_dataset, epochs=epochs, batch_size=1, validation_data=val_dataset)
 
-    # # Set the layers with non-serializable objects to None
-    # model.layer_with_eager_tensor = None
-    #
     # # Save the model
     # model.save('/home/raj/PycharmProjects/model/my_model.h5')
 
-    # Print the final accuracy.
+    # Print the final accuracy
     final_accuracy = history.history['accuracy'][-1] * 100
     print("")
-    print("Final training accuracy: {:.2f}%".format(final_accuracy))
-    # time.sleep(5)
+    print("Training accuracy: {:.2f}%".format(final_accuracy))
     print("")
 
     # Get the training accuracy and validation accuracy from the history object
@@ -195,111 +200,27 @@ def main(argv):
     # Test the model on the test dataset
     test_loss, test_accuracy = model.evaluate(test_dataset)
 
-    # # Load the saved model
-    # loaded_model = load_model('/home/raj/PycharmProjects/model/my_model.h5')
-
-    # Save the plot data
-    plot_data = {
-        'training_accuracy': training_accuracy,
-        'validation_accuracy': validation_accuracy,
-        'training_loss': training_loss,
-        'validation_loss': validation_loss,
-        'test_loss': test_loss,
-        'test_accuracy': test_accuracy
-    }
-
-    np.savez('/home/raj/PycharmProjects/frames/plot_data.npz', **plot_data)
-
-    # # Load the saved plot data
-    # loaded_data = np.load('/home/raj/PycharmProjects/frames/plot_data.npz')
-    #
-    # # Access the individual plot data
-    # training_accuracy = loaded_data['training_accuracy']
-    # validation_accuracy = loaded_data['validation_accuracy']
-    # training_loss = loaded_data['training_loss']
-    # validation_loss = loaded_data['validation_loss']
-    # test_loss = loaded_data['test_loss']
-    # test_accuracy = loaded_data['test_accuracy']
+    # Print the test loss and test accuracy
+    print("")
+    print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
+    print("")
 
     # Call the plot_accuracy_and_loss function
     plots.plot_accuracy_and_loss(training_accuracy, validation_accuracy, training_loss, validation_loss)
 
-    # # Call the plot_test_accuracy_loss function
-    # plots.plot_test_accuracy_loss(test_loss, test_accuracy)
-
     # Make predictions on the test dataset
     predictions = model.predict(test_dataset)
-
-    # Apply softmax to obtain probabilities
-    probabilities = tf.nn.softmax(predictions)
-
-    # Convert probabilities to a NumPy array
-    probabilities_array = probabilities.numpy()
-
-    # Normalize probabilities to ensure they total to 100%
-    normalized_probabilities = probabilities_array / np.sum(probabilities_array, axis=1, keepdims=True)
-
-    # Extract probabilities of being healthy and ill for each video
-    healthy_probabilities = normalized_probabilities[:, 1]
-    ill_probabilities = normalized_probabilities[:, 0]
-
-    # Print the probabilities for each video
-    for i in range(len(healthy_probabilities)):
-        print(f"Video {i + 1}: Probability of being healthy = {healthy_probabilities[i] * 100:.2f}%, Probability of being ill = {ill_probabilities[i] * 100:.2f}%")
-
-    # Find the most accurate healthy and ill probabilities
-    max_healthy_prob_index = np.argmax(healthy_probabilities)
-    max_ill_prob_index = np.argmax(ill_probabilities)
-
-    # Print the video indices with the most accurate probabilities
-    print(f"Video index with highest healthy probability: {max_healthy_prob_index + 1}")
-    print(f"Video index with highest ill probability: {max_ill_prob_index + 1}")
 
     # Call the plot_predictions function
     plots.plot_predictions(predictions, test_videos_tensor)
 
-    # def generate_image_overlay(video_frames):
-    #     # Create an empty canvas for the overlay image
-    #     overlay_image = np.zeros_like(video_frames[0], dtype=np.float32)
-    #
-    #     # Iterate through each frame and overlay it on the canvas
-    #     for frame in video_frames:
-    #         overlay_image = cv2.addWeighted(overlay_image, 1, frame.astype(np.float32), 1, 0)
-    #
-    #     return overlay_image.astype(np.uint8)
-    #
-    # # Select a healthy video
-    # healthy_video_frames = test_native_videos[0]
-    # healthy_overlay_image = generate_image_overlay(healthy_video_frames)
-    #
-    # # Select a modified video
-    # mod_video_frames = test_modified_videos[0]
-    # mod_overlay_image = generate_image_overlay(mod_video_frames)
-    #
-    # # Create a canvas to hold the final plot
-    # plot_canvas = np.zeros((healthy_overlay_image.shape[0], healthy_overlay_image.shape[1] + 200, 3), dtype=np.uint8)
-    # plot_canvas[:healthy_overlay_image.shape[0], :healthy_overlay_image.shape[1]] = healthy_overlay_image
-    #
-    # # Draw bars indicating native accuracy and mod accuracy
-    # native_accuracy = model.predict(np.expand_dims(test_videos_tensor[0], axis=0))[0][0]
-    # mod_accuracy = model.predict(np.expand_dims(test_videos_tensor[video_index], axis=0))[0][1]
-    #
-    # bar_height = int(plot_canvas.shape[0] / 2)
-    # native_bar_width = int(native_accuracy * plot_canvas.shape[1])
-    # mod_bar_width = int(mod_accuracy * plot_canvas.shape[1])
-    #
-    # plot_canvas[:bar_height, healthy_overlay_image.shape[1]:] = (0, 255, 0)  # Native bar (green)
-    # plot_canvas[bar_height:, healthy_overlay_image.shape[1]:] = (0, 0, 255)  # Mod bar (red)
-    #
-    # plot_canvas[:bar_height, healthy_overlay_image.shape[1]:healthy_overlay_image.shape[1] + native_bar_width] = (0, 128, 0)  # Filled part of native bar
-    # plot_canvas[bar_height:, healthy_overlay_image.shape[1]:healthy_overlay_image.shape[1] + mod_bar_width] = (0, 0, 128)  # Filled part of mod bar
-    #
-    # # Display and save the final plot
-    # cv2.imwrite('/home/raj/PycharmProjects/frames/overlay_plot.jpg', plot_canvas)
+    # Show the plots
+    plt.show()
 
-    # Print the test loss and test accuracy.
-    print("Test loss: {:.2f}".format(test_loss))
-    print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
+    # Saved plots
+    print("")
+    print("Plots saved")
+    print("")
 
 
 if __name__ == "__main__":
--- rbc_classifier/plots.py
+++ rbc_classifier/plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 
 save_directory = '/home/raj/PycharmProjects/frames/'
 
@@ -15,36 +16,38 @@ def plot_accuracy_and_loss(training_accuracy, validation_accuracy, training_loss
     # Plot the training accuracy on the first subplot
     ax1.plot(epochs, training_accuracy, 'b', label='Training Accuracy')
     ax1.plot(epochs, validation_accuracy, 'r', label='Validation Accuracy')
-    ax1.set_title('Training Accuracy vs Validation Accuracy')
-    ax1.set_xlabel('Epochs')
-    ax1.set_ylabel('Accuracy')
-    ax1.legend()
+    ax1.set_title('Accuracy: Training vs Validation', fontsize=20)
+    ax1.set_xlabel('Epochs', fontsize=16)
+    ax1.set_ylabel('Accuracy', fontsize=16)
+    ax1.legend(fontsize=16)
 
     # Plot the training loss on the second subplot
     ax2.plot(epochs, training_loss, 'b', label='Training Loss')
     ax2.plot(epochs, validation_loss, 'r', label='Validation Loss')
-    ax2.set_title('Training Loss vs Validation Loss')
-    ax2.set_xlabel('Epochs')
-    ax2.set_ylabel('Loss')
-    ax2.legend()
+    ax2.set_title('Loss:  Training vs Validation', fontsize=20)
+    ax2.set_xlabel('Epochs', fontsize=16)
+    ax2.set_ylabel('Loss', fontsize=16)
+    ax2.legend(fontsize=16)
 
     # Adjust the spacing between subplots
     plt.subplots_adjust(wspace=0.3)
+
+    plt.show()
 
     # Save the plot as an EPS file
     plt.savefig(save_directory + 'accuracy_and_loss_plot.eps', format='eps')
     plt.close()
 
-
-# def plot_test_accuracy_loss(test_loss, test_accuracy):
-#     plt.plot(1, test_loss, 'bo', label='Test Loss')
-#     plt.plot(1, test_accuracy, 'ro', label='Test Accuracy')
-#     plt.title('Test Accuracy and Loss')
-#     plt.xlabel('Epochs')
-#     plt.ylabel('Metric')
-#     plt.legend()
-#     plt.savefig(save_directory + 'test_accuracy_loss_plot.eps', format='eps')
-#     plt.close()
+    # # Save the values as a CSV file
+    # data = {
+    #     'Epochs': epochs,
+    #     'Training Accuracy': training_accuracy,
+    #     'Validation Accuracy': validation_accuracy,
+    #     'Training Loss': training_loss,
+    #     'Validation Loss': validation_loss
+    # }
+    # df = pd.DataFrame(data)
+    # df.to_csv(save_directory[:-4] + '_acc_loss.csv', index=False)
 
 
 def overlay(video, save_directory):
@@ -60,124 +63,87 @@ def overlay(video, save_directory):
     # Save the preprocessed image
     cv2.imwrite(save_directory, enhanced_image)
 
+    # Return the enhanced image
     return enhanced_image
 
 
-def plot_overlay_and_bar(image, probabilities, save_path):
-    # Create the figure and subplots
-    fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+def plot_bar_chart(probabilities, save_path):
+    # Define colors for the bars
+    colors = ['red', 'green']
 
-    # Plot the overlay image
-    axs[0].imshow(image)
-    axs[0].set_title('Overlay')
-    axs[0].axis('off')
+    # Create a figure with the specified size
+    plt.figure(figsize=(2, 6))
 
-    # Plot the bar chart for the probabilities
-    axs[1].bar(['Healthy', 'Ill'], probabilities, color=['green', 'red'])
-    axs[1].set_ylim([0, 100])
-    axs[1].axis('off')
+    # Plot the healthy probability bar
+    plt.bar(0.1, probabilities[1], color=colors[1], width=0.3)
 
-    # Remove spacing between subplots
-    plt.subplots_adjust(wspace=0)
+    # Plot the ill probability bar on top of the healthy bar
+    plt.bar(0.1, probabilities[0], bottom=probabilities[1], color=colors[0], width=0.3)
 
-    # Save the figure
+    # Set the y-axis limits
+    plt.ylim([0, 1])
+
+    # Remove ticks
+    plt.xticks([])
+    plt.yticks([])
+
+    # Save the plot to the specified path
     plt.savefig(save_path)
+
+    # Close the plot
+    plt.close()
+
+    # # Save the values as a CSV file
+    # data = {
+    #     'Probability': ['Ill', 'Healthy'],
+    #     'Value': probabilities
+    # }
+    # df = pd.DataFrame(data)
+    # df.to_csv(save_path[:-4] + '_values.csv', index=False)
 
 
 def plot_predictions(predictions, test_videos_tensor):
     # Convert TensorSliceDataset to list
     test_videos_list = list(test_videos_tensor)
 
-    # Find the index of the most accurate healthy and ill probabilities
-    max_healthy_index = np.argmax(predictions[:, 1])
-    max_ill_index = np.argmax(predictions[:, 0])
+    # Apply softmax to obtain probabilities
+    probabilities = tf.nn.softmax(predictions)
 
-    # Extract the videos with the most accurate healthy and ill probabilities
-    max_healthy_video = test_videos_list[max_healthy_index]
-    max_ill_video = test_videos_list[max_ill_index]
-
-    # Preprocess the videos and save the images
-    healthy_img = overlay(max_healthy_video, save_directory + 'max_healthy_video.png')
-    ill_img = overlay(max_ill_video, save_directory + 'max_ill_video.png')
-
-    # Get the probabilities of being healthy and ill for the selected videos
-    healthy_probabilities = predictions[max_healthy_index].astype(np.float32)
-    ill_probabilities = predictions[max_ill_index].astype(np.float32)
+    # Convert probabilities to a NumPy array
+    probabilities_array = probabilities.numpy()
 
     # Normalize probabilities to ensure they total to 100%
-    healthy_probabilities = healthy_probabilities / np.sum(healthy_probabilities) * 100
-    ill_probabilities = ill_probabilities / np.sum(ill_probabilities) * 100
+    normalized_probabilities = probabilities_array / np.sum(probabilities_array, axis=1, keepdims=True)
 
-    # Plot overlay and bar for the most accurate healthy video
-    plot_overlay_and_bar(healthy_img, [healthy_probabilities[1], healthy_probabilities[0]], save_directory + 'max_healthy_predictions.png')
+    # Convert the probabilities to float32
+    normalized_probabilities = normalized_probabilities.astype(np.float32)
 
-    # Plot overlay and bar for the most accurate ill video
-    plot_overlay_and_bar(ill_img, [ill_probabilities[1], ill_probabilities[0]], save_directory + 'max_ill_predictions.png')
+    # Convert the probabilities to a list
+    probabilities_list = normalized_probabilities.tolist()
 
+    # Find the indices of the top three healthy and ill probabilities
+    top_healthy_indices = np.argsort(probabilities_list, axis=0)[-3:, 1]
+    top_ill_indices = np.argsort(probabilities_list, axis=0)[-3:, 0]
 
+    # Extract the videos with the top three healthy and ill probabilities
+    top_healthy_videos = [test_videos_list[index] for index in top_healthy_indices]
+    top_ill_videos = [test_videos_list[index] for index in top_ill_indices]
 
+    # Preprocess the videos and save the images
+    for i, video in enumerate(top_healthy_videos):
+        overlay(video, save_directory + f'top_healthy_video_{i}.png')
 
+    for i, video in enumerate(top_ill_videos):
+        overlay(video, save_directory + f'top_ill_video_{i}.png')
 
-# def plot_predictions(predictions, test_videos_tensor):
-#     # Convert TensorSliceDataset to list
-#     test_videos_list = list(test_videos_tensor)
-#
-#     # Find the index of the most accurate healthy and ill probabilities
-#     max_healthy_index = np.argmax(predictions[:, 0])
-#     max_ill_index = np.argmax(predictions[:, 1])
-#
-#     # Extract the videos with the most accurate healthy and ill probabilities
-#     max_healthy_video = test_videos_list[max_healthy_index]
-#     max_ill_video = test_videos_list[max_ill_index]
-#
-#     # Preprocess the videos and save the images
-#     overlay(max_healthy_video, save_directory + 'max_healthy_video.png')
-#     overlay(max_ill_video, save_directory + 'max_ill_video.png')
-#
-#     # Get the probabilities of being healthy and ill for the selected videos
-#     max_healthy_probabilities = predictions[max_healthy_index].astype(np.float32)
-#     max_ill_probabilities = predictions[max_ill_index].astype(np.float32)
-#
-#     # Create the figure and subplots
-#     fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-#
-#     # Load the preprocessed images
-#     enhanced_image_max_healthy = cv2.imread(save_directory + 'max_healthy_video.png')
-#     enhanced_image_max_ill = cv2.imread(save_directory + 'max_ill_video.png')
-#
-#     # Plot the healthy video image
-#     axs[0, 0].imshow(enhanced_image_max_healthy)
-#     axs[0, 0].set_title('Most Accurate Healthy Video')
-#     axs[0, 0].axis('off')
-#
-#     # Plot the bar chart for the probabilities of being healthy or ill for the healthy video
-#     axs[0, 1].bar(['Healthy'], max_healthy_probabilities)
-#     axs[0, 1].text(0, 0.5, f'Probability: {max_healthy_probabilities[0]:.2%}', verticalalignment='center')
-#     axs[0, 1].set_title('Probability Distribution (Healthy Video)')
-#     axs[0, 1].set_ylabel('Probability')
-#     axs[0, 1].set_ylim([0, 1])
-#     axs[0, 1].set_ylim(axs[0, 1].get_ylim()[::-1])  # Reverse the y-axis to match image orientation
-#
-#     # Scale the bar chart to match the height of the healthy video image
-#     image_height = enhanced_image_max_healthy.shape[0]
-#     scale_factor = image_height / 132
-#     axs[0, 1].set_ylim([0, 1 * scale_factor])
-#
-#     # Plot the ill video image
-#     axs[1, 0].imshow(enhanced_image_max_ill)
-#     axs[1, 0].set_title('Most Accurate Ill Video')
-#     axs[1, 0].axis('off')
-#
-#     # Plot the bar chart for the probabilities of being healthy or ill for the ill video
-#     axs[1, 1].bar(['Ill'], max_ill_probabilities)
-#     axs[1, 1].text(0, 0.5, f'Probability: {max_ill_probabilities[0]:.2%}', verticalalignment='center')
-#     axs[1, 1].set_title('Probability Distribution (Ill Video)')
-#     axs[1, 1].set_ylabel('Probability')
-#     axs[1, 1].set_ylim([0, 1])
-#
-#     # Adjust the spacing between subplots
-#     plt.tight_layout()
-#
-#     # Save the figure
-#     plt.savefig(save_directory + 'predictions.png')
+    # Get the probabilities of being healthy and ill for the selected videos
+    top_healthy_probabilities = normalized_probabilities[top_healthy_indices]
+    top_ill_probabilities = normalized_probabilities[top_ill_indices]
 
+    # Create the bar charts for the top three healthy videos
+    for i in range(len(top_healthy_videos)):
+        plot_bar_chart(top_healthy_probabilities[i], save_directory + f'top_healthy_bar_chart_{i}.png')
+
+    # Create the bar charts for the top three ill videos
+    for i in range(len(top_ill_videos)):
+        plot_bar_chart(top_ill_probabilities[i], save_directory + f'top_ill_bar_chart_{i}.png')
